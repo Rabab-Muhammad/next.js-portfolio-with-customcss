2dd92857e2bd413703eaab7d3d43ac01c6579421
Rabab-Muhammad <rababmuhammadhussain@gmail.com>
2024-11-11 23:53:36 +0500

    updated

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import "../app/styles/card.css";
 
@@ -10,31 +10,42 @@ interface propsType {
 }
 
 const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // This will set to true once the component is mounted in the client-side
+  }, []);
+
   return (
     <div className="card-container">
-    <div className={`card ${window.innerWidth >= 640 ? 'card-sm' : '' }`} data-aos="zoom-in-up">
-      <div>
-        <Image
-          className={`card-image ${window.innerWidth >= 640 ? 'card-image-sm' : ''}`}
-          src={img}
-          height={300}
-          width={300}
-          alt={title}
-        />
-      </div>
-
-      <div className="card-content">
-        <div className="card-title">{title}</div>
-        <div>{desc}</div>
+      <div
+        className={`card ${isClient && window.innerWidth >= 640 ? 'card-sm' : ''}`}
+        data-aos="zoom-in-up"
+      >
         <div>
-          {tags.map((el) => (
-            <div className="card-tags" key={el}>
-              {el}
-            </div>
-          ))}
+          {isClient && (
+            <Image
+              className={`card-image ${window.innerWidth >= 640 ? 'card-image-sm' : ''}`}
+              src={img}
+              height={300}
+              width={300}
+              alt={title}
+            />
+          )}
+        </div>
+
+        <div className="card-content">
+          <div className="card-title">{title}</div>
+          <div>{desc}</div>
+          <div>
+            {tags.map((el) => (
+              <div className="card-tags" key={el}>
+                {el}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
